ac7e102ed6b77e1c995388fb3a6d42d2c260a687
shadowhunter5773-create <shadowhunter5773@gmail.com>
2026-08-28 21:33:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -73,6 +73,7 @@ const SITE_CONFIG = {
     { platform: "TikTok",    image: "https://cdn.simpleicons.org/tiktok/ffffff",    url: "https://www.tiktok.com/@kami.kung?_r=1&_t=ZS-98jU7xuWtkX" },
     { platform: "HIWDO",     image: "https://cdn.simpleicons.org/paypal/ffffff", url: "https://hiwdo.com/kamikung" },
     { platform: "ENDEXSHOP",     image: "https://cdn.simpleicons.org/shopify/ffffff", url: "https://endexshop.xezw.xyz/categories/fpbknE0HON9f" }
+    { platform: "กลุ่มซื้อขาย",     image: "https://cdn.simpleicons.org/facebook/ffffff", url: "https://www.facebook.com/share/g/1F5wuVasNz/" }
   ],
 
   /* -------------------- MUSIC PLAYLIST -------------------- */
